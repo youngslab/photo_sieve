1374b0632bdc476f4f3d55ddda5d5f1fb18198f0
Jaeyoung Park <jaeyoungs.park@gmail.com>
2019-10-19 23:45:17 +0900

    refactor functions

--- sieve.py
+++ sieve.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
+
+
 import os
 import os.path
 import sys
 import shutil
 
-def  generate_files ( dir):
+def  generate_files (dir):
     result = []
     for (subdir, _, fs) in os.walk(dir):
         for f in fs:
@@ -17,6 +19,52 @@ def create_directory_if_not_exist(dir):
     if not os.path.exists(dir):
         os.mkdir(dir)
 
+def is_child(dir, filepath) :
+    if dir in filepath:
+        return True
+    else:
+        return False
+
+def search(filepaths, name):
+    for filepath in filepaths:
+        if name in filepath:
+            return True, filepath
+    return False,""
+
+def sieve(org, sel, out):
+
+    ofs = generate_files(org)
+    sfs = generate_files(sel)
+    # filter out filder alreday exists
+    sfs = [ sf for sf in sfs if not is_child(out, sf) ]
+
+    missed = []
+    count = 0
+    total = len(sfs)
+
+    for sf in sfs:
+        # get select file name only.
+        filename = os.path.splitext(os.path.basename(sf))[0]
+
+        # releative path
+        # selected file : sel/yyy/xxxx.jpg
+        # relative info : yyy
+        # 1) remove basedir()
+
+        # relative = os.path.(sf - org)
+        # serach the file name in original files.
+        found, filepath = search(ofs, filename)
+        if found:
+            # if found copy the filepath to outpu_dir
+            count+=1
+            shutil.copy2(filepath, output_dir)
+        else:
+            missed.append(filename)
+
+    return count, total, missed
+
+
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
@@ -25,28 +73,11 @@ if __name__ == "__main__":
 
     origin_dir = sys.argv[1]
     select_dir = sys.argv[2]
-
-    ofs = generate_files(origin_dir)
-    sfs = generate_files(select_dir)
-    # TODO: filter out unndessary files.
-
-    # create output directory
     output_dir = select_dir + os.sep + "raw"
     create_directory_if_not_exist(output_dir)
 
-    count = 0
-    for sf in sfs:
-        filename = os.path.splitext(os.path.basename(sf))[0]
-        found = False
-        for of in ofs:
-            if filename in of:
-                found =True
-                shutil.copy2(of, output_dir)
-                break
-        # check wheter the selected file found in raw or not.
-        if found:
-            count+=1
-        else:
-            print(" Warnig - {} is not found ".format(filename))
+    count, total, missed = sieve(origin_dir, select_dir, output_dir)
 
-    print(" Done - {}(raw) of {}(selected) are copied".format(count, len(sfs)))
+    print(" Done - {}(raw) of {}(selected) are copied".format(count, total))
+    if len(missed) != 0:
+        print(" missing files are \n {}".format(missed))
